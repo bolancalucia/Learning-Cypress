62de72196af6895f2df47b64a95e9ca09830879c
bolancalucia <bolancalucia@gmail.com>
2023-01-03 14:57:21 +0100

    First test in Cypress

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+// destructuring - getting only method defineConfig from cypress package
+// naming it the same
+
+// exporting what the defineConfig method returns, parameters are configuration property
+module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
+  e2e: {
+    setupNodeEvents(on, config) {
+      //initPlugin(on, config);
+      //on("task", percyHealthCheck);
+      //return config;
+    }
+    baseUrl: "http://localhost:4200",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    excludeSpecPattern: ["**/1-getting-started/*", "**/2-advanced-examples/*"],
+  },
+});
---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,20 @@
+describe("Our first suite", () => {
+  describe("Our suite section", () => {
+    //Hook
+    beforeEach("Code before every test", () => {
+      //repetitive code
+    });
+    it("Some test name", () => {});
+    it("Some test name", () => {});
+    it("Some test name", () => {});
+  });
+  it("First test", () => {});
+  it("Second test", () => {});
+  it("Third test", () => {});
+});
+
+describe("Our second suite", () => {
+  it("First test", () => {});
+  it("Second test", () => {});
+  it("Third test", () => {});
+});
